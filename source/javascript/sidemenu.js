7e6acb57699fa2989903091220ecf9a739713aec
ckelner <chris.kelner@gmail.com>
2017-05-15 16:42:56 -0400

    work out some js and css kinks

--- source/javascript/sidemenu.js
+++ source/javascript/sidemenu.js
@@ -1,5 +1,3 @@
----
----
 (function($) {
   $(document).ready( function() {
     var sidebarOpen = true;
@@ -8,38 +6,11 @@
     var width = $(document). width();
 
     function setSidebar() {
-      // set sidebar height
-      sidebar.height($('footer').position().top + 50);
-
       // 992 is the default breakpoint set in Bootstrap 3 for col-md
       if (width < 992)
         sidebarOpen = false;
       if (sidebarOpen == false) {
         sidebar.toggle();
-      }
-
-      // 1756 is a judgement call on the breakpoint for when the sidebar needs a col-lg-3 and not col-lg-12
-      if (width < 1756) {
-        if (sidebar.hasClass("col-lg-2"))
-          sidebar.removeClass("col-lg-2");
-        if (!sidebar.hasClass("col-lg-3"))
-          sidebar.addClass("col-lg-3");
-
-        if (content.hasClass("col-lg-10"))
-          content.removeClass("col-lg-10");
-        if (!content.hasClass("col-lg-9"))
-          content.addClass("col-lg-9");
-      }
-      else {
-        if (sidebar.hasClass("col-lg-3"))
-          sidebar.removeClass("col-lg-3");
-        if (!sidebar.hasClass("col-lg-2"))
-          sidebar.addClass("col-lg-2");
-
-        if (content.hasClass("col-lg-9"))
-          content.removeClass("col-lg-9");
-        if (!content.hasClass("col-lg-10"))
-          content.addClass("col-lg-10");
       }
     }
 
