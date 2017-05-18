7c4790d38417ea531e2d915493b1123eb814825f
ckelner <chris.kelner@gmail.com>
2017-05-18 14:01:43 -0400

    Refactor build script to handle releases

--- build.sh
+++ build.sh
@@ -9,6 +9,10 @@
 set -ex
 PARENT_DIR=$(pwd)
 
+# Array of iternal releases on https://github.ibm.com/blueprint/bluemix-terraform-provider-dev
+INTERNAL_RELEASES=("ibmcloud-v0.1-beta" "ibmcloud-v0.2-beta" \
+  "ibmcloud-v0.3-beta" "tf-0.9.3-bluemix-api-key")
+
 # Adds arbitrary material to index
 function addtoindex() {
   # TODO: Kelner - this can be fraught with failure if the text changes, and
@@ -17,23 +21,59 @@ function addtoindex() {
   mv tmp index.html.markdown
 }
 
-if [ ! -d "terraform" ]; then
-  # clone IBM terraform
-  git clone https://github.com/IBM-Bluemix/terraform --depth=1
-  cd terraform
-else
-  cd terraform
-  git pull
-fi
-# switch to the provider team base branch
-# As of May 12 2017 it is "provider/ibm-cloud"
-git checkout provider/ibm-cloud
-cp -R website/source/docs/providers/ibmcloud/* ../source
-cd ../source
-# inject contents into index
-addtoindex
+# Expects one arguement, the release tag as specified in git
+function olddocs() {
+  if [ ! -d "./source/$1" ]; then
+    mkdir ./source/$1
+  fi
+  if [ ! -d "./terraform" ]; then
+    mkdir ./terraform
+  fi
+  if [ ! -d "./terraform/$1" ]; then
+    # clone IBM terraform
+    git clone --branch $1 https://github.ibm.com/blueprint/bluemix-terraform-provider-dev --depth=1 ./terraform/$1
+    cd ./terraform/$1
+  else
+    cd ./terraform/$1
+    git pull
+  fi
+  cp -R website/source/docs/providers/ibmcloud/* ../../source/$1
+  cd ../../source/$1
+  # inject contents into index
+  addtoindex
+  cd $PARENT_DIR
+}
+
+function cleanup() {
+  if [ -d "./source/$1" ]; then
+    rm -rf ./source/$1
+  fi
+  if [ -d "./terraform" ]; then
+    # I don't like using `-f` but it is necessary in this case
+    # otherwise it will prompt the user for:
+    # override r--r--r-- for ./terraform/ibmcloud-v0.1-beta/.git/objects/pack/pack-425f29ccef3ed86df8c2e92b655158da0cefd440.idx?
+    # for every .git file
+    rm -rf ./terraform
+  fi
+}
+
+# ensure clean from start
+for release in "${INTERNAL_RELEASES[@]}"; do
+  cleanup $release
+done
+
+# pull all releases from IBM GitHub
+for release in "${INTERNAL_RELEASES[@]}"; do
+  olddocs $release
+done
+
 # build with middleman
-bundle install --no-cache --path vendor/bundle
-bundle exec middleman build --verbose --clean
+bundle install
+bundle exec middleman build --clean
+
+# cleanup
+for release in "${INTERNAL_RELEASES[@]}"; do
+  cleanup $release
+done
 
 exit 0
