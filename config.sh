2e4a8dd6ee93423089cd89363ecf3d9a726d4c03
davetropeano <davetropeano@us.ibm.com>
2018-07-11 09:18:12 -0400

    v0.11.1

--- config.sh
+++ config.sh
@@ -6,11 +6,11 @@ RELEASES=( \
   "v0.9.0" \
   "v0.9.1" \
   "v0.10.0" \
-  "v0.11.0"
+  "v0.11.1"
 )
 
 # The version of the provider that schematics is using
-LATEST_VERSION="v0.11.0"
+LATEST_VERSION="v0.11.1"
 
 # The repos that contains the terraform docs
 LEGACYREPO="https://github.com/IBM-Cloud/terraform"
